071731e912a21cefbb240bfa53ce5ab192a91263
giftajayi <162841212+giftajayi@users.noreply.github.com>
2024-11-25 19:01:43 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import pandas as pd
 st.title('GoData Vehicle Classifier')
 
 st.info('This app builds a machine learining model')
-df = pd.read_csv('https://drive.google.com/file/d/1DbHYq82ZemRz6etqJ_bq5jZRsHi4iytP/view?usp=drive_link')
+df = pd.read_csv('https://drive.google.com/file/d/1dJNBWJZAWJhzcaJDfL5ItqJKKhRX1Knl/view?usp=sharing')
 df
